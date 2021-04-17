d3bc9a39e7943b11a0c665f0c71857788490ce06
DARAEDomPc <4romad67@solent.ac.uk>
2021-04-17 21:48:09 +0100

    update

--- public/map.js
+++ public/map.js
@@ -3,7 +3,6 @@ function map() {
 }
 
 function initMap{
-
 	const map = L.map ("map1");
 
 	const attrib="Map data copyright OpenStreetMap contributors, Open Database Licence";
@@ -20,40 +19,30 @@ function initMap{
 	});
 }
 
-
-const solent = L.circle([50.9079, -1.4015], { radius:100, fillColor: 'blue',
+function addModulesToMap() {
+	const solent = L.circle([50.9079, -1.4015], { radius:100, fillColor: 'blue',
                                 color: 'red', opacity: 0.5 }).addTo(map);
 
-// Saints stadium (football ground)
-const saints = L.polygon ( [
+	// Saints stadium (football ground)
+	const saints = L.polygon ( [
         [50.9063 , -1.3914 ] ,
         [50.9063 , -1.3905 ] ,
         [50.9053 , -1.3905 ] ,
         [50.9053 , -1.3914 ]
         ] ).addTo(map);
 
-// Route to railway station
-const routeToStation = L.polyline ( [
+	// Route to railway station
+	const routeToStation = L.polyline ( [
         [50.9079, -1.4015] ,
         [50.9071, -1.4015], 
         [50.9069, -1.4047],
         [50.9073, -1.4077],
         [50.9081, -1.4134] 
         ]).addTo(map);
-const marker = L.marker(pos).addTo(map);
+	const marker = L.marker(pos).addTo(map);
 
-solent.bindPopup("Solent University");
-saints.bindPopup("Saints stadium");
-routeToStation.bindPopup("Route to station");
-marker.bindPopup("My Location");
-
-// Custom markers
-const bondstreet = L.polygon([[51.314018690847135, -2.215558290481568] ,
-							[51.3143539934763, -2.215955257415772] ,
-							[51.31464905776292, -2.2152364253997807] ,
-							[51.314467995811995, -2.2151935100555424]]).addTo(map);
-
-bondstreet.bindPopup("Gruba Kurwa street");
-
-const ita = L.circle([41.90354188154937, 12.477996842935683], { radius: 20000, fillColor: 'pink', color: 'pink', opacity: 0.8}).addTo(map);
-ita.bindPopup("Unicorn City");
+	solent.bindPopup("Solent University");
+	saints.bindPopup("Saints stadium");
+	routeToStation.bindPopup("Route to station");
+	marker.bindPopup("My Location");
+}
