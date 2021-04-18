afa6930ca679cc91110b21a9b1fb5c1b6e23270d
DARAEDomPc <4romad67@solent.ac.uk>
2021-04-18 22:59:29 +0100

    update

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const path = require('path');
 const ejs = require("ejs");
 
 const serverPort=process.env.SERVER_PORT;
-const conn = require('./routes/mysqlconn');
-const routes = require('./routes/users');
-const dataRouter = require('./routes/dataRouter');
 
 require('dotenv').config(); 
 const app = express();
@@ -18,11 +15,20 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public"))); 
 app.use(express.urlencoded({extended: true}));
 
+const conn = require('./routes/mysqlconn');
+const routes = require('./routes/users');
+const dataRouter = require('./routes/dataRouter');
+
 // own middleware
 //app.use('/user', (req, res, next) => {
 //		console.log(`Received a request in time ${Date.now()} ms.`);
 //		next();
 //});
+
+
+app.use('/user', routes);
+
+app.use('/poi', dataRouter);
 
 app.get('/home', (req, res) => {
 		conn.query(`SELECT * FROM pointsofinterest ORDER BY ID DESC LIMIT 10`, 
@@ -38,65 +44,60 @@ app.get('/home', (req, res) => {
 		);
 });
 
-app.get('/poi/find/:region', (req, res) => {
-		conn.query(`SELECT * FROM pointsofinterest WHERE region=?`, [req.params.region], 
-		(error, results, fields) => {
-				if (error) {
-						res.status(500).json({error:error});
-				} else {
-						res.json(
-								results
-						);
-				}
-		});
-});
+//app.get('/poi/find/:region', (req, res) => {
+//		conn.query(`SELECT * FROM pointsofinterest WHERE region=?`, [req.params.region], 
+//		(error, results, fields) => {
+//				if (error) {
+//						res.status(500).json({error:error});
+//				} else {
+//						res.json(
+//								results
+//						);
+//				}
+//		});
+//});
 
-app.get('/poi/find/recent/region', (req, res) => {
-		conn.query(`SELECT * FROM pointsofinterest WHERE region=? ORDER BY ID DESC LIMIT 10`, [req.params.region],
-		(error, results, fields) => {
-				if (error) {
-						res.status(500).json({error:error});
-				} else {
-						res.json(
-								results
-						);
-				}
-		});
-});
+//app.get('/poi/find/recent/region', (req, res) => {
+//		conn.query(`SELECT * FROM pointsofinterest WHERE region=? ORDER BY ID DESC LIMIT 10`, [req.params.region],
+//		(error, results, fields) => {
+//				if (error) {
+//						res.status(500).json({error:error});
+//				} else {
+//						res.json(
+//								results
+//						);
+//				}
+//		});
+//});
 
-app.post('/poi/add', (req, res) => {
+//app.post('/poi/add', (req, res) => {
 	//	if (checkJSON(req.body.name)) {
 	//			console.log(`poi/add Success`);
-		conn.query(`INSERT INTO pointsofinterest( name, type, country, region, lon, lat, description) VALUES(?, ?, ?, ?, ?, ?, ?)`, [req.body.name, req.body.type, req.body.country, req.body.region, req.body.lon, req.body.lat, req.body.description], 
-		(error, results, fields) => {
-				if (error) {
-						res.status(500).json({error:error});
-				} else {
-						res.json({success:1});
-				}
-		});
+//		conn.query(`INSERT INTO pointsofinterest( name, type, country, region, lon, lat, description) VALUES(?, ?, ?, ?, ?, ?, ?)`, [req.body.name, req.body.type, req.body.country, req.body.region, req.body.lon, req.body.lat, req.body.description], 
+//		(error, results, fields) => {
+//				if (error) {
+//						res.status(500).json({error:error});
+//				} else {
+//						res.json({success:1});
+//				}
+//		});
 	//	} else {
 	//			console.log(`poi/add Fail`);
 	//	}
-});
+//});
 
-app.post('/poi/:id/recommend', (req, res) => {
-		conn.query(`UPDATE pointsofinterest SET recommendations=recommendations+1 WHERE id=?`, [req.params.id],
-		(error, results, fields) => {
-				if(error) {
-						res.status(500).json({error:error});
-				} else if(results.affectedRows==1) {
-						res.json({'message': 'Recomendation sent!'});
-				} else {
-						res.status(404).json({error: 'ID not found!'});
-				}
-		});
-});
-
-app.use('/user', routes);
-
-app.use('/poi', dataRouter);
-
+//app.post('/poi/:id/recommend', (req, res) => {
+//		conn.query(`UPDATE pointsofinterest SET recommendations=recommendations+1 WHERE id=?`, [req.params.id],
+//		(error, results, fields) => {
+//				if(error) {
+//						res.status(500).json({error:error});
+//				} else if(results.affectedRows==1) {
+//						res.json({'message': 'Recomendation sent!'});
+//				} else {
+//						res.status(404).json({error: 'ID not found!'});
+//				}
+//		});
+//});
 console.log(`Server is running on http://localhost:${process.env.SERVER_PORT}/`);
 app.listen(8080);
 
--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -2,13 +2,16 @@ const express = require('express');
 const dataRouter = express.Router();
 const conn = require('./mysqlconn');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+dataRouter.use(bodyParser.json());
+dataRouter.use(express.urlencoded({extended: true}));
 
 const poiControl = require('../controllers/poi');
 
 dataRouter.use(cors());
 
 // works
-//dataRouter.get('/poi/find0/:region', (req, res) => {
+//dataRouter.get('/poi/find/:region', (req, res) => {
 //		conn.query(`SELECT * FROM pointsofinterest WHERE region=?`, [req.params.region], 
 //		(error, results, fields) => {
 //				if (error) {
@@ -23,9 +26,10 @@ dataRouter.get('/poi/find/:region', poiControl.search);
 
 dataRouter.post('/poi/:id/recommend', poiControl.recommend);
 
+dataRouter.post('/poi/add', poiControl.add);
+
 dataRouter.get('/poi/recent', poiControl.recent);
 
-dataRouter.post('poi/add', poiControl.add);
 
 // change, currently works for ejs
 dataRouter.get('/poi/home', (req, res) => {
