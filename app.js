78c29b874bd009ff4a160e0f83dda42fc3a00a9c
DARAEDomPc <4romad67@solent.ac.uk>
2021-04-19 15:56:08 +0100

    update

--- app.js
+++ app.js
@@ -44,18 +44,18 @@ app.get('/home', (req, res) => {
 		);
 });
 
-//app.get('/poi/find/:region', (req, res) => {
-//		conn.query(`SELECT * FROM pointsofinterest WHERE region=?`, [req.params.region], 
-//		(error, results, fields) => {
-//				if (error) {
-//						res.status(500).json({error:error});
-//				} else {
-//						res.json(
-//								results
-//						);
-//				}
-//		});
-//});
+app.get('/poi/find/:region', (req, res) => {
+		conn.query(`SELECT * FROM pointsofinterest WHERE region=?`, [req.params.region], 
+		(error, results, fields) => {
+				if (error) {
+						res.status(500).json({error:error});
+				} else {
+						res.json(
+								results
+						);
+				}
+		});
+});
 
 //app.get('/poi/find/recent/region', (req, res) => {
 //		conn.query(`SELECT * FROM pointsofinterest WHERE region=? ORDER BY ID DESC LIMIT 10`, [req.params.region],
