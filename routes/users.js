c3d6419345b0254e8d95e3d8224a173152efdf89
DARAEDom <4romad67@solent.ac.uk>
2021-04-26 11:14:42 +0100

    login success

--- routes/users.js
+++ routes/users.js
@@ -2,29 +2,11 @@ const express = require('express');
 const userRoutes = express.Router();
 const conn = require('./mysqlconn');
 
-const expressSession = require('express-session');
-const MySQLStore = require('express-mysql-session')(expressSession);
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 const sessionStore = new MySQLStore({ } , conn.promise());
 
-userRoutes.post('/login', (req, res) => {
-	console.log(`${req.body.username} Username, ${req.body.password} Password`);
-	conn.query(`SELECT * FROM poi_users WHERE username = ? AND password = ?`, [req.body.username, req.body.password], 
-	(error, results, fields) => {
-		if(results.length > 0) {
-			req.body.username = req.session.username;
-			res.json({success: 1});
-		}
-/*		if(req.body.username == 'username' && req.body.password == 'password') {
-			req.session.username = req.body.username;
-			res.json({success: 1});
-*/
-		else {
-			res.status(401).json({error: "Incorrect Login!"});
-		}
-	});
-});
-
-userRoutes.use(expressSession({
+userRoutes.use(session({
     store: sessionStore, 
 
     secret: 'rapsberry_pie', 
@@ -38,6 +20,28 @@ userRoutes.use(expressSession({
         httpOnly: false 
     }
 }));
+
+userRoutes.post('/login', (req, res) => {
+	console.log(`${req.body.username} Username, ${req.body.password} Password`);
+	conn.query(`SELECT * FROM poi_users WHERE username = ? AND password = ?`, [req.body.username, req.body.password], 
+	(error, results, fields) => {
+		if(results.length > 0) {
+			console.log(req.body.username);
+			req.session.username = req.body.username;
+			res.json({success: 1});
+		} else if (error) {
+			res.status(404).json({error: error});
+		}
+/*		if(req.body.username == 'username' && req.body.password == 'password') {
+			req.session.username = req.body.username;
+			res.json({success: 1});
+*/
+		else {
+			res.status(401).json({error: "Incorrect Login!"});
+		}
+	});
+});
+
 
 userRoutes.post('/logout', (req, res ) => {
 	req.session == null;
